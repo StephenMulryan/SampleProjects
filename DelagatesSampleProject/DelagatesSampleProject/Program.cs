82818ecff5eaee9d7e9afc99cdb80329d4ab00b4
Stephen Mulryan <annagheast@gmail.com>
2013-04-08 23:00:22 +0100

    Adding Sample project to show a delegate being used

---
+++ DelagatesSampleProject/DelagatesSampleProject/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    
+    /// <summary>
+    /// Example to show the use of a delegate
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Declaration of delegate with method signature defined.
+        /// </summary>
+        /// <param name="info"> input parameter type</param>
+        /// <returns>return type will be a string</returns>
+        delegate string Del(string info);
+
+        /// <summary>
+        /// Declaring an anonymous method which can be assigned to the delegate.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        static string Print(string name) { return "Your name is " + name; }
+
+        static void Main(string[] args)
+        {
+            // Assign the delegate to the Print method.
+            Del d = Print;
+            //output what the method has returned through the delegate.
+            Console.WriteLine(d("Stephen Mulryan"));
+            Console.ReadLine();
+            
+        }
+    }
+}
